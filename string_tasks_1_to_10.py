b0fe46e9404daa903bb5940ccb29154391ef9b67
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-01 16:17:45 +0300

    Second task

--- string_tasks_1_to_10.py
+++ string_tasks_1_to_10.py
@@ -1,3 +1,5 @@
+# 1
+
 a = 'qwerty'
 
 for i in range(3):
@@ -5,3 +7,16 @@ for i in range(3):
         print(a)
     else:
         print(a, end=', ')
+
+
+# 2
+
+a = 'qwertyu'
+
+k = len(a)
+m = int(k / 2)
+
+if k % 2 == 0:
+    print(a[0: k: k - 1])
+else:
+    print(a[0: k: m])
