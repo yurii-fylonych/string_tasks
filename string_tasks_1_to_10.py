880ef1f2c73f360cabde5c8277f6e82eecd23621
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-01 16:30:23 +0300

    Third task

--- string_tasks_1_to_10.py
+++ string_tasks_1_to_10.py
@@ -20,3 +20,16 @@ if k % 2 == 0:
     print(a[0: k: k - 1])
 else:
     print(a[0: k: m])
+
+# 3
+
+a = 'qwer'
+
+k = len(a)
+
+if k <= 5:
+    for i in range(k + 1):
+        print(a[0], end=' ')
+
+else:
+    print(a[0: 3], a[k - 3: k])
