4da80f1930572e3d4461bd066db18429192b8aac
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-05 20:42:12 +0300

    32-th task

--- string_tasks_31_to_40.py
+++ string_tasks_31_to_40.py
@@ -7,3 +7,19 @@ k = int(input())
 string_without_el = string[:k - 1] + string[k:]
 
 print(string_without_el)
+
+
+# 32
+
+first_string = '1234567'
+second_string = '4335'
+
+if len(first_string) > len(second_string):
+    result = first_string.find(second_string)
+else:
+    result = second_string.find(first_string)
+
+if result == -1:
+    print('First string does not include second string')
+else:
+    print('First string includes second string')
