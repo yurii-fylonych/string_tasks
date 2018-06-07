a1d3dabf355d92a7f5d3676d78546bb743c5da3f
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-07 09:01:57 +0300

    Change 42-th task

--- string_tasks_41_to_50.py
+++ string_tasks_41_to_50.py
@@ -9,3 +9,20 @@ if condition == False:
     print('String does not quantity')
 else:
     print('String is quantity')
+
+
+# 42
+
+string = 'ab      aldsdedqweqwqwe   abag   kjtertrertfdjaba'
+
+lst = string.split()
+
+max_len_word = 0
+max_word = ''
+
+for word in lst:
+    if len(word) > max_len_word:
+        max_len_word = len(word)
+        max_word = word
+
+print(max_word)
