b884b9b22b80f795b9db8725eb021a54ce6409a1
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-09 21:08:12 +0300

    44-th tasks

--- string_tasks_41_to_50.py
+++ string_tasks_41_to_50.py
@@ -44,3 +44,40 @@ d = {1: 'I',
      }
 
 print(d[int(string)])
+
+
+# 44.2
+
+string = 'loloolo@gmail.ua'
+
+n = len(string)
+condition = True
+
+dog_pos = string.find('@')
+count_dot = string[dog_pos:].count('.')
+dot_pos = string.rfind('.')
+login = string[:dog_pos]
+company = string[dog_pos + 1: dot_pos]
+tail = string[dot_pos + 1:]
+len_tail = len(tail)
+len_login = len(login)
+len_company = len(company)
+
+if len_login < 1 or login[len_login - 1].isalnum() == False:
+    condition = False
+elif string.count('@') != 1 or string.count(' ') != 0:
+    condition = False
+elif company.isalpha() == False or len_company == 0:
+    condition = False
+elif dot_pos == -1 or count_dot != 1 or (dog_pos - dot_pos) == 1:
+    condition = False
+elif len_tail < 2  or tail.isalpha() == False:
+    condition = False
+else:
+    condition = True
+
+
+if condition == True:
+   print('Correct email')
+else:
+    print('Try again')
