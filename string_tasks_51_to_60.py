86a6292435f7d5cc751fe2736061a5b17e4b5519
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-10 14:14:26 +0300

    52-th tasks

--- string_tasks_51_to_60.py
+++ string_tasks_51_to_60.py
@@ -12,3 +12,31 @@ for char in string:
 result = string_with_signes
 
 print(result)
+
+
+# 52
+
+import random
+
+string = 'It'
+digit_letters = '1234567890qwertyuiopasdfghjklzxcvbnm'
+
+individ_char = ''.join(set(string))
+
+if len(individ_char) >= 3:
+    random_chars = ''.join(random.sample(individ_char, 3))
+    for char in random_chars:
+        string = string.replace(char, '', 1)
+else:
+    char_list = list(string)
+    diff_chars = ''.join(set(digit_letters).difference(string))
+    random_chars = ''.join(random.sample(diff_chars, 3))
+    k = 0
+    for i in random_chars:
+        random_pos = random.randint(0, len(string) + k)
+        char_list.insert(random_pos, i)
+        k += 1
+        print(char_list)
+    string = ''.join(char_list)
+
+print()
