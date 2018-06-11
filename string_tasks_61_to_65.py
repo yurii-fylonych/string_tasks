f294778a48a8477dc5678ecac8590446dad79d21
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-11 10:21:46 +0300

    62-th tasks

--- string_tasks_61_to_65.py
+++ string_tasks_61_to_65.py
@@ -8,3 +8,25 @@ pos_dot = string.rfind('.')
 result = string[pos_slash + 1: pos_dot]
 
 print(result)
+
+
+# 62
+
+string = 'abcdxs'
+
+word_list = list(string)
+list_in_order = sorted(word_list)
+
+i = 0
+condition = True
+
+while i < len(word_list) and condition == True :
+    if word_list[i] != list_in_order[i]:
+        print(word_list[i], end=' ')
+        condition = False
+    i += 1
+
+if condition == False:
+    print('Incorrect alphabet order')
+else:
+    print('Yes')
